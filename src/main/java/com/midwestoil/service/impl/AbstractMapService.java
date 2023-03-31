d8561ac22ba8359ab01cf538989f3405b7229227
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-03-30 18:24:16 -0600

    added AbstractMapService

---
+++ src/main/java/com/midwestoil/service/impl/AbstractMapService.java
@@ -0,0 +1,24 @@
+package com.midwestoil.service.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class AbstractMapService<ID, T> {
+
+    protected Map<ID, T> map = new HashMap<>();
+
+    T save(ID id, T object){
+        map.put(id, object);
+        return object;
+    }
+
+    List<T> findAll() {
+        return new ArrayList<>(map.values());
+    }
+
+    T findById(ID id) {
+        return map.get(id);
+    }
+}
